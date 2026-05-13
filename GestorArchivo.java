75ae8dc227484c926b2dab6e8ef42c8218e687da
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-05-12 18:29:43 -0600

    crear gestor de lectura

--- GestorArchivo.java
+++ GestorArchivo.java
@@ -1,3 +1,17 @@
+import java.io.*;
+
 public class GestorArchivo {
-    
+    public static void cargarGrafo(String rutaArchivo, Grafo grafo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(" ");
+                if (partes.length == 3) {
+                    grafo.agregarRuta(partes[0], partes[1], Double.parseDouble(partes[2])); 
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("No se pudo cargar el archivo: " + e.getMessage());
+        }
+    }
 }
