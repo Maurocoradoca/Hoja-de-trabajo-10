849aa3dd5a236a0c140ec690c15e73345735c51c
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-05-12 18:15:37 -0600

    implementar algoritmo de excentricidad para calcular el centro

--- CalculadoraCentro.java
+++ CalculadoraCentro.java
@@ -1,3 +1,25 @@
 public class CalculadoraCentro {
-    
+    public static int encontrarCentro(double[][] matrizDistancias, int numNodos) {
+        double[] excentricidades = new double[numNodos];
+        
+        for (int j = 0; j < numNodos; j++) {
+            double maxColumna = 0;
+            for (int i = 0; i < numNodos; i++) {
+                if (matrizDistancias[i][j] > maxColumna && matrizDistancias[i][j] != Double.POSITIVE_INFINITY) {
+                    maxColumna = matrizDistancias[i][j];
+                }
+            }
+            excentricidades[j] = maxColumna;
+        }
+
+        double minExcentricidad = Double.POSITIVE_INFINITY;
+        int centro = -1;
+        for (int j = 0; j < numNodos; j++) {
+            if (excentricidades[j] < minExcentricidad) {
+                minExcentricidad = excentricidades[j];
+                centro = j;
+            }
+        }
+        return centro;
+    }
 }
